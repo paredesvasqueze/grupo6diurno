2bbe542d48451fff82021a282da3793a8fa6c69e
maribeljulca <julcaverac@iestptrujillo.net>
2024-10-07 10:05:13 -0500

    commit de Usuario

---
+++ BackEnd/CapaEntidad/Usuario.cs
@@ -0,0 +1,22 @@
+﻿namespace CapaEntidad
+{
+    public class columnas
+    {            
+        public int nId_Usuario { get; set; }
+        public string cNombre { get; set; }
+        public string cApellido { get; set; }
+        public int dtelefono { get; set; }
+
+        public int cdni  { get; set; }
+
+    }
+
+
+    public class claves
+    {
+        public
+    }
+
+
+
+}
